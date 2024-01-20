ef26cbab6006601e702b0b4eeb2d4e2e3b059638
Faiaz Hossain <faiazhossain10@gmail.com>
2024-01-20 21:05:43 +0600

    feat: click to fly added

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,14 @@
-"use client"
+"use client";
 
-import React, { useState, useRef, useEffect, useCallback } from 'react'
-import 'maplibre-gl/dist/maplibre-gl.css';
+import React, { useState, useRef, useEffect, useCallback } from "react";
+import "maplibre-gl/dist/maplibre-gl.css";
 const MAP_KEY = process.env.NEXT_PUBLIC_MAP_API_ACCESS_TOKEN;
-import Map, {GeolocateControl,GeolocateResultEvent} from 'react-map-gl/maplibre';
+import Map, { GeolocateControl } from "react-map-gl/maplibre";
 
 export default function Home() {
-  const mapRef = useRef(null)
-  const map = mapRef.current
-  const [centerPoint,setCenterPoint] = useState([]);
+  const mapRef = useRef(null);
+  const map = mapRef.current;
+  const [centerPoint, setCenterPoint] = useState([]);
   const geoControlRef = useRef();
 
   useEffect(() => {
@@ -17,49 +17,49 @@ export default function Home() {
       map?.flyTo({
         center: centerPoint,
         essential: true,
-        zoom: 13
-      })
+        zoom: 13,
+      });
     }
-  }, [centerPoint, map])
+  }, [centerPoint, map]);
 
+  // useEffect(() => {
+  //   // Add Barikoi marker on map load
+  //   if (map) {
+  //     const marker = new bkoigl.Marker({ draggable: true })
+  //       .setLngLat([90.3938010872331, 23.821600277500405])
+  //       .addTo(map);
+  //   }
+  // }, [map]);
 
-  
-  useEffect(() => {
-    // Add Barikoi marker on map load
-    if (map) {
-      const marker = new bkoigl.Marker({ draggable: true })
-        .setLngLat([90.3938010872331, 23.821600277500405])
-        .addTo(map);
-    }
-  }, [map]);
-
-  const clickToFly = ()=>{
+  const clickToFly = () => {
     setCenterPoint([90.36402004477634, 23.823730671721]);
-  }
+  };
 
   useEffect(() => {
-    console.log('clicked')
+    console.log("clicked");
     geoControlRef.current?.trigger();
   }, [centerPoint]);
 
-  const clickedLocation = ()=>{
-    console.log('clicked')
-  }
   return (
-    <main>
+    <main style={{ position: "relative" }}>
       <Map
-      // ref={ mapRef }
-      initialViewState={{
-        longitude: 90,
-        latitude: 23,
-        zoom: 6
-      }}
-      style={{width: '98vw', height: '98vh'}}
-      mapStyle="https://map.barikoi.com/styles/osm-liberty/style.json?key=NDE2NzpVNzkyTE5UMUoy"
-    >
-      <GeolocateControl style={{fontSize:"48px"}}/>
-      </Map>   
-    <button onClick={clickToFly}>Click</button>
+        ref={mapRef}
+        initialViewState={{
+          longitude: 90,
+          latitude: 23,
+          zoom: 6,
+        }}
+        style={{ width: "98vw", height: "94vh" }}
+        mapStyle="https://map.barikoi.com/styles/osm-liberty/style.json?key=NDE2NzpVNzkyTE5UMUoy"
+      >
+        <GeolocateControl style={{ fontSize: "48px" }} />
+      </Map>
+      <button
+        style={{ position: "absolute", top: "20px" }}
+        onClick={clickToFly}
+      >
+        Click to fly
+      </button>
     </main>
-  )
+  );
 }
