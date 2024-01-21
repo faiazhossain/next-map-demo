8b50e7274181e423077ed4dd0276ab9e7c8f088c
nurmdrafi <nurmdrafi2014@gmail.com>
2024-01-21 11:22:40 +0600

    fix: marker duplicate issue

--- MapComponent/Map.js
+++ MapComponent/Map.js
@@ -1,45 +1,67 @@
 "use client";
-import React, { useRef, useEffect, useState } from "react";
+
+// Import Components
+import React, { useRef, useEffect, useState, useCallback } from "react";
+
+// Import Styles
 import "./Map.css";
 
 // Provide the api key to your .env.local file
 const MAP_KEY = process.env.NEXT_PUBLIC_MAP_API_ACCESS_TOKEN;
 
-export default function Map() {
-  const mapContainer = useRef(null);
-  const map = useRef(null);
+const MapGL = () => {
+  // States
+  const mapContainerRef = useRef(null);
+  const mapRef = useRef(null);
   const [lng] = useState(90.36402);
   const [lat] = useState(23.823731);
   const [zoom] = useState(13);
   const [API_KEY] = useState(MAP_KEY);
 
-  useEffect(() => {
-    if (map.current) return; // stops map from intializing more than once
-
-    map.current = new window.bkoigl.Map({
-      container: mapContainer.current,
+  // Create Map
+  const _createMap = useCallback(() => {
+    mapRef.current = new window.bkoigl.Map({
+      container: mapContainerRef.current,
       style: `https://map.barikoi.com/styles/osm-liberty/style.json`,
       center: [lng, lat],
       zoom: zoom,
       accessToken: `${API_KEY}`,
     });
-  }, [API_KEY, lng, lat, zoom]);
 
-  useEffect(() => {
-    map.current.on("load", () => {
+    // Add Controls
+    mapRef.current.addControl(new window.bkoigl.NavigationControl(), 'bottom-right')
+    mapRef.current.addControl(new window.bkoigl.FullscreenControl())
+
+    // On Load
+    mapRef.current.on("load", () => {
+      // Add marker
       const marker = new window.bkoigl.Marker({
-        // draggable: true, //if you want to drag the marker
+        draggable: true, // if you want to drag the marker
         color: "green",
       })
         .setLngLat([lng, lat])
-        .addTo(map.current);
-      // .setRotation(45); // if you want to rotate the marker
+        .setRotation(10) // if you want to rotate the marker
+        .addTo(mapRef.current);
     });
-  });
+  }, [API_KEY, lat, lng, zoom])
+
+  // Destroy Map
+  const _destroyMap = () => {
+    // Remove Map Instance
+    mapRef.current.remove()
+  }
+
+  useEffect(() => {
+    _createMap();
+    return () => {
+      _destroyMap();
+    }
+  }, [API_KEY, lng, lat, zoom, _createMap]);
 
   return (
     <div className="map-wrap">
-      <div ref={mapContainer} className="map" />
+      <div ref={mapContainerRef} className="map" />
     </div>
   );
 }
+export default MapGL
